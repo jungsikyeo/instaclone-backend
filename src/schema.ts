7c69a5ac563d8bed0775c783b9dcc6d2062123c1
jungsikyeo <mdotdom12@naver.com>
2021-08-07 02:51:56 +0900

    typescript add & createAccount, editProfile, login, seeProfile add

--- src/schema.ts
+++ src/schema.ts
@@ -5,10 +5,8 @@ import {
   mergeTypeDefs,
 } from "graphql-tools";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
-);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.ts`);
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
+import client from "./client";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      client,
+    };
+  },
 });
 
 const PORT = process.env.PORT;
